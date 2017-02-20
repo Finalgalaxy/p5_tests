c1411a7eb0edd51d964a527c6e386546b97debb0
Finalgalaxy <finalgalaxy@gmail.com>
2017-02-20 21:37:04 +0100

    Added text to center with gradient.

--- p5-zip/example/sketch.js
+++ p5-zip/example/sketch.js
@@ -1,26 +1,30 @@
 var system;         // Particle system.
 var incremental;    // Optimizer for calculating offsets.
 var list;           // List of trajectories for particle system.
+var background_color;
+var text_pos;
 
 function setup() {
     createCanvas(720, 400);
     system = new ParticleSystem(createVector(width/2, 50));
+    background_color=51;
     
     list=[]; // List of trajectories data for X animations (2 trajectories).
     incremental=[];
+    text_pos=null;
     
     /* Make 2 trajectories, opposite ones on X axis.
     */
     list[0] = new PSTrajectory(
         createVector(0,0),              // Start from (0,0)...
         createVector(width,height),     // End at bottom-right corner (width,height)...
-        30,                             // This parameter increases number of circles generated (more CPU)...
+        50,                             // This parameter increases number of circles generated (more CPU)...
         true                            // true=Right direction!
     );
     list[1] = new PSTrajectory(
         createVector(width,0),          // Start from the top-right corner (width,0)...
         createVector(0,height),         // End at bottom-left corner (0,height)...
-        30,                             // Same as above trajectory...
+        50,                             // Same as above trajectory...
         false                           // false=Left direction!
     );
     
@@ -36,8 +40,11 @@ function setup() {
     }
 }
 
+
+
+
 function draw() {
-    background(51);
+    background(background_color);
     for(var i=0;i<list.length;i++){ // For each trajectory...
         if( 
             (   
@@ -49,12 +56,34 @@ function draw() {
                 list[i].current.y < (list[i].end.y - incremental[i].y)  // Check if height is finished.
             ){
                 system.addParticle(createVector(list[i].current.x,list[i].current.y));
+                if(checkCollisions()){
+                    text_pos=createVector(list[i].current.x,list[i].current.y);
+                    color_changer();    // Recursive functions by using setTimeout.
+                }
                 list[i].current.x+=incremental[i].x;
                 list[i].current.y+=incremental[i].y;
             }
     }
-    
+    if(text_pos!=null){
+        textSize(70);
+        fill(0, 102, 153, ((background_color-51)/(200-51))*100);
+        text("Yay! :'D", text_pos.x-100, text_pos.y);
+    }
     system.run();
+}
+
+// Recursive function: each 5ms tries background_color++, if background_color reaches max value, recursion stops.
+var color_changer=function(){
+    if(background_color<200){
+        background_color++;
+        setTimeout(color_changer,5);    // Recursion!
+    }
+}
+
+function checkCollisions(){
+    return (Math.abs(list[0].current.x - list[1].current.x) <= 20
+        &&
+        Math.abs(list[0].current.y - list[1].current.y) <= 20);
 }
 
 /* Particle-System-Trajectory. A trajectory is defined by:
